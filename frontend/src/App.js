47c9bd028867bd74b63e3773d0b08e1c916f8aec
shhyunn <limgm7@gmail.com>
2023-11-04 15:45:16 +0900

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,53 @@
+import "./index.css";
+import { Sidebar } from "flowbite-react";
+import {
+  HiArrowSmRight,
+  HiChartPie,
+  HiInbox,
+  HiShoppingBag,
+  HiTable,
+  HiUser,
+  HiViewBoards,
+} from "react-icons/hi";
+
+function App() {
+  return (
+    <div className="App">
+      {/* tailwind class */}
+      <div class="fixed h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800 border-r">
+        <Sidebar aria-label="Sidebar with logo branding example">
+          <Sidebar.Logo href="#" img="/carbon.svg" imgAlt="Flowbite logo">
+            Carbon Calculator
+          </Sidebar.Logo>
+          <Sidebar.Items>
+            <Sidebar.ItemGroup>
+              <Sidebar.Item href="#" icon={HiChartPie}>
+                Dashboard
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiViewBoards}>
+                Kanban
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiInbox}>
+                Inbox
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiUser}>
+                Users
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiShoppingBag}>
+                Products
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiArrowSmRight}>
+                Sign In
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiTable}>
+                Sign Up
+              </Sidebar.Item>
+            </Sidebar.ItemGroup>
+          </Sidebar.Items>
+        </Sidebar>
+      </div>
+    </div>
+  );
+}
+
+export default App;
