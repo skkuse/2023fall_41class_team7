09280670dd88f168a40285432de4cf6e9e95abb2
dorikunz <bbty21@naver.com>
2023-11-16 23:54:02 +0900

    add color

--- frontend/src/Info.js
+++ frontend/src/Info.js
@@ -5,7 +5,7 @@ function Info() {
     <div class="flex justify-center min-h-screen pt-10">
       <div class="flex flex-col w-full max-w-7xl">
         <div class="text-3xl font-extrabold ml-10 mb-10 text-gray-800 text-center">Service Information</div>
-        <div class="container p-4 m-4 bg-white shadow-lg rounded">
+        <div class="container p-4 m-4 bg-blue-100 shadow-lg rounded">
           <h2 class="text-2xl mb-2 text-black-600 text-center">problem : CO2</h2>
           <p class="text-gray-700 mb-2 text-center">인간의 여러가지 활동으로 인해 탄소 배출이 지속적으로 증가함에 따라,</p>
           <p class="text-gray-700 mb-2 text-center">이산화탄소 농도가 높아져 지구 온난화가 가속되고 있습니다.</p>
@@ -16,7 +16,7 @@ function Info() {
 
         <div className="flex justify-evenly">
         <div className="flex-grow">
-          <div className="container p-4 m-4 bg-white shadow-lg rounded">
+          <div className="container p-4 m-4 bg-green-100 shadow-lg rounded">
             <h2 className="text-2xl mb-2 text-black-600 text-center">그린 소프트웨어(Green SW)란?</h2>
             <p className="text-gray-700 mb-2 text-center">환경 친화적인 방법으로 개발되고 운영되는 소프트웨어를 말합니다.</p>
             <p className="text-gray-700 mb-2 text-center">에너지 효율성, 재사용성, 재활용성, 그리고 지속 가능성 등을 중요한 요소로 간주합니다.</p>
@@ -26,7 +26,7 @@ function Info() {
           </div>
         </div>
         <div className="flex-grow ml-4">
-          <div className="container p-4 m-4 bg-white shadow-lg rounded">
+          <div className="container p-4 m-4 bg-pink-100 shadow-lg rounded">
             <h2 className="text-2xl mb-2 text-black-600 text-center">무엇을 제공하나요?</h2>
             <p className="text-gray-700 mb-2 text-center">사용자의 자바 소스코드의 탄소 배출량을 계산하여 결과값을 제공합니다.</p>
             <p className="text-gray-700 mb-2 text-center">결과값을 통해 사용자는 코드 최적화 작업을 진행해 볼 수 있으며,</p>
@@ -37,17 +37,17 @@ function Info() {
         </div>
       </div>
 
-        <div class="container p-4 m-4 bg-white shadow-lg rounded">
+        <div class="container p-4 m-4 bg-yellow-100 shadow-lg rounded">
           <h2 class="text-2xl mb-2 text-black-600 text-center">Formula</h2>
           <p class="text-gray-700 mb-2 text-center">계산 공식</p>
         </div>
 
-        <div class="container p-4 m-4 bg-white shadow-lg rounded">
+        <div class="container p-4 m-4 bg-indigo-100 shadow-lg rounded">
           <h2 class="text-2xl mb-2 text-black-600 text-center">입력 데이터 형식 설명</h2>
           <p class="text-gray-700 mb-2 text-center">입력 데이터 형식 설명</p>
         </div>
 
-        <div class="container p-4 m-4 bg-white shadow-lg rounded text-center">
+        <div class="container p-4 m-4 bg-red-100 shadow-lg rounded text-center">
           <h2 class="text-2xl mb-2 text-black-600 text-center">Code / More Information</h2>
           <a href="https://github.com/skkuse/2023fall_41class_team7" target="_blank" rel="noopener noreferrer" class="text-blue-500 underline">
             GitHub
