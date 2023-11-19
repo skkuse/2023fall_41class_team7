c2c1a36e0707c330a35b18d1c9b1d92a48276a0f
tlgur <begin1009@naver.com>
2023-11-19 19:36:46 +0900

    rounding return doubles

--- backend/src/main/java/com/skku/se7/dto/GreenResourceResponse.java
+++ backend/src/main/java/com/skku/se7/dto/GreenResourceResponse.java
@@ -5,10 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 @Data
 @Builder
 @NoArgsConstructor
-@AllArgsConstructor
 public class GreenResourceResponse {
     private Double totalCarbonFootprint;
     private Double totalEnergyNeeded;
@@ -16,4 +18,15 @@ public class GreenResourceResponse {
     private HwFootprint hwFootprint;
 
     private InterpretedFootprint interpretedFootprint;
+
+    public GreenResourceResponse(Double totalCarbonFootprint, Double totalEnergyNeeded, HwFootprint hwFootprint, InterpretedFootprint interpretedFootprint) {
+        this.totalCarbonFootprint = BigDecimal.valueOf(totalCarbonFootprint)
+                .setScale(3, RoundingMode.HALF_UP)
+                .doubleValue();
+        this.totalEnergyNeeded = BigDecimal.valueOf(totalEnergyNeeded)
+                .setScale(3, RoundingMode.HALF_UP)
+                .doubleValue();;
+        this.hwFootprint = hwFootprint;
+        this.interpretedFootprint = interpretedFootprint;
+    }
 }
