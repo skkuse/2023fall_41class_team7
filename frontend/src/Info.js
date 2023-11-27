389adea96e1f29bc0d6fecb28f0975f93e020867
dorikunz <bbty21@naver.com>
2023-11-27 23:39:05 +0900

    add contents

--- frontend/src/Info.js
+++ frontend/src/Info.js
@@ -4,7 +4,6 @@ function Info() {
   return (
     <div class="flex justify-center min-h-screen pt-10 info_area">
       <div class="flex flex-col w-full max-w-7xl">
-        <div class="text-3xl font-extrabold ml-10 mb-10 text-gray-800 text-center">Service Information</div>
         <div class="container p-4 m-4 bg-blue-100 shadow-lg rounded">
           <h2 class="text-3xl font-extrabold mb-2 text-black-600 text-center">Problem : CO2</h2>
           <p class="text-2xl text-gray-700 font-bold mb-2 text-center">As carbon emissions continue to increase due to various human activities,</p>
@@ -27,7 +26,7 @@ function Info() {
         </div>
         <div class="flex-grow ml-4">
           <div class="container p-4 m-4 bg-pink-100 shadow-lg rounded">
-            <h2 class="text-3xl font-extrabold mb-2 text-black-600 text-center">Services provided</h2>
+            <h2 class="text-3xl font-extrabold mb-2 text-black-600 text-center">Services Provided</h2>
             <p class="text-2xl text-gray-700 font-bold mb-2 text-center">Calculate the carbon emissions of the user's Java source code and provide the results.</p>
             <p class="text-2xl text-gray-700 font-bold mb-2 text-center">The result value allows the user to work on code optimization,</p>
             <p class="text-2xl text-gray-700 font-bold mb-2 text-center">and continuously check carbon emissions in the process.</p>
@@ -39,13 +38,29 @@ function Info() {
 
         <div class="container p-4 m-4 bg-yellow-100 shadow-lg rounded">
           <h2 class="text-3xl font-extrabold mb-2 text-black-600 text-center">Carbon Emissions Formula</h2>
-          <p class="text-2xl text-gray-700 font-bold mb-2 text-center">carbon footprint = energy needed * carbon intensity</p>
-          <p class="text-2xl text-gray-700 font-bold mb-2 text-center">runtime * (power draw for cores * usage + power draw for memory) * PUE * PSF</p>
+          <p class="text-2xl text-red-500 font-bold mb-2 text-center">carbon footprint = energy needed * carbon intensity</p>
+          <p class="text-2xl text-gray-700 font-bold mb-2 text-center">Where the energy needed is:</p>
+          <p class="text-2xl text-red-500 font-bold mb-2 text-center">runtime * (power draw for cores * usage + power draw for memory) * PUE * PSF</p>
+          <p class="text-1xl text-gray-700 font-bold mb-2 text-center">The power draw for the computing cores depends on the model and number of cores.</p>
+          <p class="text-1xl text-gray-700 font-bold mb-2 text-center">The memory power draw only depends on the size of memory available and the usage factor corrects for the real core usage.</p>
+          <p class="text-1xl text-gray-700 font-bold mb-2 text-center">The PUE (Power Usage Effectiveness) measures how much extra energy is needed to operate the data center.</p>
+          <p class="text-1xl text-gray-700 font-bold mb-2 text-center">The PSF (Pragmatic Scaling Factor) is used to take into account multiple identical runs.</p>
         </div>
 
-        <div class="container p-4 m-4 bg-indigo-100 shadow-lg rounded">
-          <h2 class="text-3xl font-extrabold mb-2 text-black-600 text-center">Input method</h2>
-          <p class="text-2xl text-gray-700 font-bold mb-2 text-center">Input method</p>
+        <div class="container p-4 m-4 bg-indigo-100 shadow-lg rounded ">
+          <h2 class="text-3xl font-extrabold mb-2 text-black-600 text-center">Input Method</h2>
+          <p class="text-3xl text-red-600 font-bold mb-2 text-center">Users can configure their hardware environment in the 'server' sector.</p>
+          <p class="text-2xl text-gray-700 font-bold mb-2 ml-2"><span style={{color: 'green'}}>Type of cores</span>: CPU, GPU, both options are available.</p>
+          <p class="text-2xl text-gray-700 font-bold mb-2 ml-2"><span style={{color: 'green'}}>number of cores</span>: Users can choose the number of cores.</p>
+          <p class="text-2xl text-gray-700 font-bold mb-2 ml-2"><span style={{color: 'green'}}>usage</span>: The ratio at which the processor operates at maximum performance. The default value is 1, and users can set a decimal value between 0 and 1 if necessary.</p>
+          <p class="text-2xl text-gray-700 font-bold mb-2 ml-2"><span style={{color: 'green'}}>model</span>: The type of model used for calculations, used to calculate Thermal Design Power (TDP) per core.</p>
+          <p class="text-2xl text-gray-700 font-bold mb-2 ml-2"><span style={{color: 'green'}}>memory</span>: The available memory, measured in GB.</p>
+          <p class="text-2xl text-gray-700 font-bold mb-2 ml-2"><span style={{color: 'green'}}>pue(Power Usage Effectiveness)</span>: Reflects the data center's power usage efficiency, the ratio of total energy consumption to the energy supplied to IT equipment. The default value is 1. </p>
+          <p class="text-2xl text-gray-700 font-bold mb-2 ml-2"><span style={{color: 'green'}}>psf(Power Scaling Factor)</span>: A power scaling coefficient used to adjust the system's power usage. The default value is 1, and users can adjust it if necessary.</p>
+          <p class="text-2xl text-gray-700 font-bold mb-2 ml-2"><span style={{color: 'green'}}>location(Location)</span>: The value necessary for calculating carbon intensity, indicating how much carbon the electricity in the corresponding country emits.</p>
+          <p class="text-3xl text-red-600 font-bold mb-2 text-center">In the 'Home' sector, users can input Java code.</p>
+          <p class="text-2xl text-gray-700 font-bold mb-2 ml-2"><span style={{color: 'green'}}>Clear button</span>: Users can reset the written Java code.</p>
+          <p class="text-2xl text-gray-700 font-bold mb-2 ml-2"><span style={{color: 'green'}}>Submit button</span>: Users can save their code and hardware specifications in JSON format to interact with the backend and check the calculated carbon emissions.</p>
         </div>
 
         <div class="container p-4 m-4 bg-red-100 shadow-lg rounded text-center">
