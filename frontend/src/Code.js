3487b60ac119efdaf10e057c08d0d9f14088e353
kimminsuu <kms990825@naver.com>
2023-12-03 22:48:52 +0900

    Code section edit

--- frontend/src/Code.js
+++ frontend/src/Code.js
@@ -2,26 +2,77 @@ import React from "react";
 import "./index.css";
 import "./Code.css";
 import AceEditor from "react-ace";
-import {useEffect, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import "ace-builds/src-noconflict/mode-java";
 import "ace-builds/src-noconflict/theme-monokai";
 import "ace-builds/src-noconflict/ext-language_tools";
+import { HWContext, HWProvider } from "./Hardware";
+
 
 function onChange(newValue) {
   console.log("change", newValue);
 }
 
+function APICall() {
+  const { HWValue, setHWValue } = useContext(HWContext);
+  useEffect(() => {
+    const SendDataToServer = async () => {
+      try {
+        const sender = {
+          HWValue
+        };
+        alert(sender);
+        const response = await fetch("http://localhost:8080/green", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(sender),
+        });
 
+        if (!response.ok) {
+          throw new Error(`HTTP error, status : ${response.status}`);
+        }
 
-function Code() {
+        const responseData = await response.json();
+        console.log("Response data:", responseData);
+      } catch (error) {
+        console.error("Fetching error: ", error);
+      }
+    };
+
+    SendDataToServer();
+  }, []);
+}
+
+function Innercomponent() {
   const [height, setHeight] = useState(500);
   let [fade, setFade] = useState("");
-
-  const onClick=()=> {
-    setHeight(1000);
+  const { HWValue, setHWValue } = useContext(HWContext);
+  const [javaCode, setJavaCode] = useState(`public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
   }
+}`);
+  const onClick = () => {
+    setHeight(1000);
+  };
+
+  const BtnClick = () => {
+    setHWValue({
+      ...HWValue,
+      javaCode:javaCode,
+    });
+
+    APICall();
+  };
 
   const onClear=()=> {
+    setJavaCode(`public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello!");
+  }
+}`);
     setHeight(500);
   }
 
@@ -34,7 +85,7 @@ function Code() {
 
   return (
     <div className="w-full">
-      <div className={'code_area start '+fade} onClick={onClick}>
+      <div className={'code_area start '} onClick={onClick}>
         
         <AceEditor
           height={height}
@@ -43,16 +94,16 @@ function Code() {
           mode="java"
           theme="monokai"
           name="blah2"
+
           onChange={onChange}
           // onLoad={this.onLoad}
           // onChange={this.onChange}
-          fontSize={16}
+          fontSize={24}
           showPrintMargin={true}
           showGutter={true}
           highlightActiveLine={true}
-          value={`function onLoad(editor) {
-    console.log("i've loaded");
-  }`}
+          value={javaCode}
+          
           setOptions={{
             enableBasicAutocompletion: false,
             enableLiveAutocompletion: false,
@@ -66,11 +117,17 @@ function Code() {
       <div style={{display: 'flex', justifyContent: "right", gap:10, padding:10}}>
         <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600" >Upload</button>
         <button className="rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-600" onClick={onClear}>Clear</button>
-        <button className="rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-600">Submit</button>
+        <button className="rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-600" onClick={BtnClick}>Submit</button>
       </div>
-      
-      
     </div>
+  );
+}
+
+function Code() {
+  return (
+    <HWProvider>
+      <Innercomponent />
+    </HWProvider>
   );
 }
 
